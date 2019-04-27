462fc9915ff62bfae1a257cf93ad4a6cc7173b9c
gabriela <glm@cin.ufpe.br>
2019-04-27 20:18:50 -0300

    Adding the solution to question 05

---
+++ questao05/LinkedList.java
@@ -0,0 +1,53 @@
+package questao05;
+import questao05.Node;
+
+public class LinkedList {
+	
+	Node head;
+	
+	public void insertNode(LinkedList list, String value) {
+		Node newNode = new Node(value);
+		if(list.head == null) {
+			list.head = newNode;
+		} else {
+			Node currentNode = list.head;
+			while(currentNode.next != null) {
+				currentNode = currentNode.next;
+				}
+				currentNode.next = newNode;	
+		}
+	}
+	
+	public void printList(LinkedList list) {
+		Node currentNode = list.head;
+		while(currentNode != null) {
+			System.out.print(currentNode.value);
+			currentNode = currentNode.next;
+		}
+	}
+	
+	public void removeDuplicate(LinkedList lista) {
+		Node currentNode = lista.head;
+		Node nextNode = currentNode.next;
+		Node prevNode = currentNode;
+		while(currentNode != null) {
+			while (nextNode != null) {
+				if (currentNode.value.equals(nextNode.value)) {
+					prevNode.next = nextNode.next;
+					nextNode = nextNode.next;
+				} else {					
+					prevNode = nextNode;
+					nextNode = nextNode.next;
+				}
+			}
+			
+			currentNode = currentNode.next;
+			prevNode = currentNode;
+			if(currentNode != null) {			
+				nextNode = currentNode.next;
+			}
+		}
+		
+	}
+	
+}
---
+++ questao05/Main.java
@@ -0,0 +1,28 @@
+package questao05;
+import java.util.Scanner;
+import questao05.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		LinkedList lista = new LinkedList();
+		for(int i = 0; i < 8; i++) {
+			System.out.println("Say something: ");
+			String x = in.next();
+			lista.insertNode(lista, x);
+			lista.printList(lista);
+			System.out.println("\n");
+		}
+		
+		System.out.println("This is the initial list: ");
+		lista.printList(lista);
+		System.out.println("\n");
+		lista.removeDuplicate(lista);
+		System.out.println("After removing the duplicates this is the list now: ");
+		lista.printList(lista);
+
+		in.close();
+	}
+}
